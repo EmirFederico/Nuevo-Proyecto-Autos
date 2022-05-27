3049cc9df709686475786ee5baa48b51aefaafc4
EmirFederico <emirfederico@gmail.com>
2022-05-27 18:21:26 -0300

    se agregan rutas app.get basicas

--- app.js
+++ app.js
@@ -1,16 +1,24 @@
 const express = require("express");
 const app = express();
-const path = require("path");
+const path = require ("path");
 
+const publicPath = path.resolve(__dirname, "./public");
+app.use(express.static(publicPath));
 
-app.get('/', (req,res) => {
-    res.send("Hola mundo")
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+    console.log("servidor corriendo en puerto http://localhost:3000")
 })
 
-/*app.get('/', (req,res) => {
-    res.sendFile(path.join(__dirname, poner rutaaaaaa ))
-})*/
+app.get("/", (req, res) => {
+    res.sendFile(path.resolve(__dirname,"./views/home.html"));
+})
 
-app.listen((3000), () => {
-    console.log("servidor corriendo en localhost:3000")
-});
+app.get("/register.html", (req, res) => {
+    res.sendFile(path.resolve(__dirname,"./views/register.html"));
+})
+
+app.get("/login.html", (req, res) => {
+    res.sendFile(path.resolve(__dirname,"./views/login.html"));
+})
