0a69bf3b7d5628a9be9665b88354db8cceba875f
EmirFederico <emirfederico@gmail.com>
2022-05-27 18:55:05 -0300

    se agrega html esqueleto Home

--- app.js
+++ app.js
@@ -15,10 +15,10 @@ app.get("/", (req, res) => {
     res.sendFile(path.resolve(__dirname,"./views/home.html"));
 })
 
-app.get("/register.html", (req, res) => {
+app.get("/register", (req, res) => {
     res.sendFile(path.resolve(__dirname,"./views/register.html"));
 })
 
-app.get("/login.html", (req, res) => {
+app.get("/login", (req, res) => {
     res.sendFile(path.resolve(__dirname,"./views/login.html"));
 })
